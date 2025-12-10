b38368e660677ff0a45a18bd7acd0b6092487c7d
Ryiro <redgearryiro@gmail.com>
2025-12-11 02:48:50 +0530

    go mod getting replaced by crlf?

---
+++ internal/config/config.go
@@ -0,0 +1,38 @@
+package config
+
+import (
+	"flag"
+	"log"
+	"os"
+)
+
+type HTTPServer struct {
+	Addr string
+}
+
+type Config struct {
+	Env         string `yaml:"env" env:"ENV" env-required:"true" env-default:"production"`
+	StoragePath string `yaml:"storage_path" env-required:"true"`
+	HTTPServer  `yaml:"http_server"`
+}
+
+func MustLoad() {
+	var configPath string
+	configPath = os.Getenv("CONFIG_PATH")
+
+	if configPath == "" {
+		flags := flag.String("config", "", "Path to config file")
+		flag.Parse()
+
+		configPath = *flags
+	}
+
+	if configPath == "" {
+		log.Fatal("config path is not provided")
+
+	}
+
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		log.Fatalf("config file does not exist at path: %s", configPath)
+	}
+}
